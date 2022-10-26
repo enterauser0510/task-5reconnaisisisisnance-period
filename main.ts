408ba606020a3195dc96b8298fecc9a8bbb1e054
enterauser0510 <115065002+enterauser0510@users.noreply.github.com>
2022-10-26 14:19:57 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+let michelangelo = 0
+let da_vinci = 0
+input.onButtonPressed(Button.A, function () {
+    michelangelo = 0
+    da_vinci = 0
+    for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(michelangelo, da_vinci)
+            michelangelo += 1
+            basic.pause(200)
+        }
+        da_vinci += 1
+        michelangelo = 0
+    }
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
