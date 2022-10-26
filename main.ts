e0ddcbdab7d1004bb1508848b343e066c69399e5
enterauser0510 <115065002+enterauser0510@users.noreply.github.com>
2022-10-26 14:44:38 +0800

    20 minutes of pain

--- main.ts
+++ main.ts
@@ -14,6 +14,27 @@ input.onButtonPressed(Button.A, function () {
     }
     basic.clearScreen()
 })
+input.onButtonPressed(Button.B, function () {
+    michelangelo = 0
+    da_vinci = 0
+    for (let index = 0; index < 3; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(michelangelo, da_vinci)
+            michelangelo += 1
+            basic.pause(200)
+        }
+        da_vinci += 1
+        michelangelo = 4
+        for (let index = 0; index < 5; index++) {
+            led.plot(michelangelo, da_vinci)
+            michelangelo += -1
+            basic.pause(200)
+        }
+        da_vinci += 1
+        michelangelo = 0
+    }
+    basic.clearScreen()
+})
 basic.forever(function () {
 	
 })
